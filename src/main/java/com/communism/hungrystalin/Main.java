a7f40f696c6c38c09f4a91afbe3ebe94d7edc221
abias <abias1122@gmail.com>
2022-02-08 03:31:12 -0500

    Add comically large spoon item with texture

--- src/main/java/com/communism/hungrystalin/Main.java
+++ src/main/java/com/communism/hungrystalin/Main.java
@@ -1,9 +1,11 @@
 package com.communism.hungrystalin;
 
+import com.communism.hungrystalin.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -17,18 +19,23 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-@Mod("hungrystalin")
+@Mod(Main.MOD_ID)
 public class Main
 {
+    public static final String MOD_ID = "hungrystalin";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Main() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        modEventBus.addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        modEventBus.addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/communism/hungrystalin/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.communism.hungrystalin.item;
+
+import com.communism.hungrystalin.Main;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
+
+    public static final RegistryObject<Item> COMICALLY_LARGE_SPOON = ITEMS.register("comically_large_spoon",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
