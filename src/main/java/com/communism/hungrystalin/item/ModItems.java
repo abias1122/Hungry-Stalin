5fdddc92f9af3cb7af5c2880450733a42afeb454
abias <abias1122@gmail.com>
2022-02-16 22:39:03 -0500

    Add spawn egg for Stalin

--- src/main/java/com/communism/hungrystalin/item/ModItems.java
+++ src/main/java/com/communism/hungrystalin/item/ModItems.java
@@ -3,6 +3,7 @@ package com.communism.hungrystalin.item;
 import com.communism.hungrystalin.Main;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,6 +16,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> COMICALLY_LARGE_SPOON = ITEMS.register("comically_large_spoon",
             () -> new ComicallyLargeSpoonItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static final RegistryObject<ForgeSpawnEggItem> STALIN_SPAWN_EGG = ITEMS.register("stalin_spawn_egg",
+            () -> new StalinSpawnEgg(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/communism/hungrystalin/item/StalinSpawnEgg.java
@@ -0,0 +1,10 @@
+package com.communism.hungrystalin.item;
+
+import com.communism.hungrystalin.entity.ModEntities;
+import net.minecraftforge.common.ForgeSpawnEggItem;
+
+public class StalinSpawnEgg extends ForgeSpawnEggItem {
+    public StalinSpawnEgg(Properties props) {
+        super(ModEntities.STALIN, 0xFFFFFF, 0xFFFFFF, props);
+    }
+}
